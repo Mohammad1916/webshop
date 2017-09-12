cf2adaead4b186b8d5cf5087e408b060ddf555a7
Mohammad1916 <30621593+Mohammad1916@users.noreply.github.com>
2017-09-12 15:46:35 +0200

    wenshop

---
+++ WebshopApp/WebshopApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebshopApp.Startup))]
+namespace WebshopApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
